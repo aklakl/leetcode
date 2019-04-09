73a468fb8dd9216d8b6489804fb831b7e320ab97
MingLi <ming.li@robustwealth.com>
2019-04-09 01:07:28 -0400

    add idea for 79

--- leetCode/src/com/leetcode/l35/Solution.java
+++ leetCode/src/com/leetcode/l35/Solution.java
@@ -31,4 +31,15 @@ public class Solution {
         return nums.length;
     }
 
+    
+    public int searchInsert1(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
 }
