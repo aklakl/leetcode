1718b30e1ce9104c003d7f7a4431fd39547470bc
MingLi <ming.li@robustwealth.com>
2019-04-16 00:54:50 -0400

    707. Design Linked List

---
+++ leetCode/src/com/leetcode/l707/MyLinkedList.java
@@ -0,0 +1,148 @@
+package com.leetcode.l707;
+
+import com.others.datastructure.node.ListNode;
+
+class MyLinkedList {
+	
+	ListNode node;
+	ListNode tail;
+	int size = 0;
+
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+        
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+    	if (index >= size || index < 0) return -1; 
+    	int i = 0 ;
+    	ListNode cur = node;
+    	while(i < index ) {
+    		cur = cur.next;
+    		i++;
+    	}
+		return cur.val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+    	ListNode newNode = new ListNode(val);
+    	newNode.next = node;
+    	if (node == null) {
+    		tail = newNode;
+    	}
+    	node = newNode;
+    	size++;
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        if (tail == null) return;
+        ListNode newNode = new ListNode(val);
+        tail.next = newNode;
+        tail = newNode;
+        size++;
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+    	if (index >= size || index < 0) return ;
+    	if (index ==0) {
+    		this.addAtHead(val);
+    		return ;
+    	}
+    	int i = 0 ;
+    	ListNode newNode = new ListNode(val);
+    	ListNode cur = node;
+    	while(i < index-1 ) {
+    		cur = cur.next;
+    		i++;
+    	}
+    	ListNode next = cur.next;
+    	cur.next = newNode;
+    	newNode.next = next;
+    	size++;
+    }
+    
+    public void deleteHead() {
+    	node = node.next;
+		size--;
+		return ;
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+    	if (index >= size || index < 0) return ;
+    	if (index ==0) {
+    		this.deleteHead();
+    		return;
+    	}
+    	int i = 0;
+    	ListNode curr = node;
+    	while(i < index-1 ) {
+    		curr = curr.next;
+    		i++;
+    	}
+    	ListNode next = curr.next.next;
+    	curr.next = next;
+    	size--;
+    }
+    
+    public static void main(String[] arg) {
+    	testing2();
+    }
+    
+    public static void testing2() {
+    	MyLinkedList linkedList = new MyLinkedList();
+    	linkedList.addAtHead(1);
+    	linkedList.addAtTail(3);
+    	linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	int result = linkedList.get(1);            // returns 2
+    	System.out.println("result=" + result);
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	linkedList.deleteAtIndex(1);  // now the linked list is 1->3
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	result = linkedList.get(1);            // returns 3
+    	System.out.println("result=" + result);
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	
+    	linkedList.addAtIndex(0, 0);  // linked list becomes 0-1->3
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	
+    	linkedList.deleteAtIndex(2);  // linked list becomes 0-1 
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	linkedList.deleteAtIndex(0);  // linked list becomes 0 
+    	System.out.println("node="+linkedList.node.toString()+"|s="+linkedList.size);
+    	
+    }
+    
+    public static void testing() {
+   	 MyLinkedList obj = new MyLinkedList();
+   	 for(int i = 0;i<10 ; i++) {
+   		 obj.addAtHead(i);
+   	 }
+   	 System.out.println("node="+obj.node.toString()+"|s="+obj.size);
+   	 obj.addAtTail(11);
+   	 System.out.println("node="+obj.node.toString()+"|s="+obj.size);
+   	 obj.addAtIndex(5,5);
+   	 System.out.println("node="+obj.node.toString()+"|s="+obj.size);
+   	 obj.deleteAtIndex(5);
+   	 System.out.println("node="+obj.node.toString()+"|s="+obj.size);
+   	 int param_1 = obj.get(5);
+   	 System.out.println(param_1);
+   	 System.out.println("node="+obj.node.toString()+"|s="+obj.size);
+   	 
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
